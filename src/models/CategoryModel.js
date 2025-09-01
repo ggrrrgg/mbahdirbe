d62ad85ebe376ada4dd225ba261a4f46117c0612
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2025-09-01 13:41:56 +1000

    category model up

---
+++ src/models/CategoryModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, unique: true, trim: true },
+  description: { type: String, trim: true }
+});
+
+const Category = mongoose.model('Category', CategorySchema);
+
+module.exports = { Category }

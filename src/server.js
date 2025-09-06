d27234ac6c75feb1a83e4678e4988e60e5d73dd1
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2025-09-07 07:03:43 +1000

    server update include controllers

--- src/server.js
+++ src/server.js
@@ -24,9 +24,29 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const BusinessRouter = require('./controllers/BusinessController');
+app.use('/business', BusinessRouter);
+
+const CategoryRouter = require('./controllers/CategoryController');
+app.use('/category', CategoryRouter);
+
+const UserRouter = require('./controllers/UserController');
+app.use('/users', UserRouter);
+
 app.listen(PORT, HOST, () => {
   console.log(`🚀 Server running at http://${HOST}:${PORT}`);
 });
 
-// const UserRouter = require('./controllers/UserController');
-// app.use('/users', UserRouter);
+// test message to see the api is running
+app.get("/", (request, response) => {
+
+	response.send("Hello world, this server is hello!");
+
+});
+
+module.exports = {
+	app,
+	HOST,
+	PORT
+}
+
